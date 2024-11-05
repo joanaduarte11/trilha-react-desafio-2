fc9a765c780ddeab1a9ef84114c1b2fd7af0b397
joanaduarte11 <holandajoana99@gmail.com>
2024-11-05 02:40:01 -0300

    resolução do desafio github wiki da DIO

---
+++ src/components/Button/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+export const ButtonContainer = styled.button`
+
+    background-color: #fafafa;
+    border: 1px solid #fafafa;
+    border-radius: 20px;
+
+    height: 50px;
+    width: 30%;
+
+    margin: 20px;
+
+    &: hover {
+        background-color: #fafafa40;
+        cursor: pointer;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const InputContainer = styled.div`
+
+    border: 1px solid #fafafa;
+    border-radius: 20px;
+
+    height: 62px;
+    width: 80%;
+
+    margin: 20px;
+
+    input {
+        background: transparent;
+        border: 0;
+        width: 70%;
+        height: 50px;
+        padding: 0 20px;
+        color: #fff;
+        font-size: 20px;
+    }
+`
